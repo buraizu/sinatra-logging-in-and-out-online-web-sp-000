371c1d92708c788f02b385dcba49a224fb8c1727
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-17 02:28:03 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
       @user = Helpers.current_user(session)
 
       erb :account
-    elsif Helpers.is_logged_in?(session) == false
+    elsif !Helpers.is_logged_in?(session)e
       erb :error
     end
   end
